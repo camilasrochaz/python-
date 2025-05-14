85cd97b473491e8fb783a25fcdbb51faa99fcabb
isadraw <camila.vinticinco@escola.pr.gov.br>
2025-05-14 10:34:14 -0300

    commit
    commit exemplo 1 variaveis

---
+++ pythonProject/capitulo 1/progama 1.py
@@ -0,0 +1,21 @@
+# variaveis nao podem comecar com numeros
+## 7trombones = 'big parede'
+
+# variaveis nao podem comecar com letras maiusculas
+# more = 100000
+
+# variaveis nao poem ter caracteres especiais
+#tirano 0 _ underline
+# more@= 1000
+
+#operador de soma
+soma = 1 + 2
+
+#operador de subtraçao
+subtraçao = 2 - 1
+
+#operador de multiplicaçao
+multiplicaçao = 2 * 1
+
+#operador de divisao
+divisao = 2 / 1
